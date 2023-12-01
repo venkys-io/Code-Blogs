715642467b41687933840fbe5e7d8e86acf74f3a
sindhu132 <127831367+sindhu132@users.noreply.github.com>
2023-12-01 19:35:49 +0530

    Generate Parentheses.py

---
+++ Strings/Medium/Generate Parentheses.py
@@ -0,0 +1,12 @@
+def generate_parentheses(n):
+def generate(p, left, right, parens=[]):
+if left:
+generate(p + '(', left-1, right)
+if right > left:
+generate(p + ')', left, right-1)
+if not right:
+parens += p,
+return parens
+return generate('', n, n)
+
+print(generate_parentheses(3))
