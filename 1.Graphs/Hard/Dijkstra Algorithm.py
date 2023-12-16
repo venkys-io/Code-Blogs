d6a51fa5f8c14f04b2d87bc00070d63be2c9bd8c
sindhu132 <127831367+sindhu132@users.noreply.github.com>
2023-12-16 10:50:20 +0530

    Update Dijkstra Algorithm.py

--- 1.Graphs/Hard/Dijkstra Algorithm.py
+++ 1.Graphs/Hard/Dijkstra Algorithm.py
@@ -1,84 +1,47 @@
-# Python program for Dijkstra's single
-# source shortest path algorithm. The program is
-# for adjacency matrix representation of the graph
- 
-# Library for INT_MAX
-import sys
- 
- 
-class Graph():
- 
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [[0 for column in range(vertices)]
-                      for row in range(vertices)]
- 
-    def printSolution(self, dist):
-        print("Vertex \tDistance from Source")
-        for node in range(self.V):
-            print(node, "\t", dist[node])
- 
-    # A utility function to find the vertex with
-    # minimum distance value, from the set of vertices
-    # not yet included in shortest path tree
-    def minDistance(self, dist, sptSet):
- 
-        # Initialize minimum distance for next node
-        min = sys.maxsize
- 
-        # Search not nearest vertex not in the
-        # shortest path tree
-        for u in range(self.V):
-            if dist[u] < min and sptSet[u] == False:
-                min = dist[u]
-                min_index = u
- 
-        return min_index
- 
-    # Function that implements Dijkstra's single source
-    # shortest path algorithm for a graph represented
-    # using adjacency matrix representation
-    def dijkstra(self, src):
- 
-        dist = [sys.maxsize] * self.V
-        dist[src] = 0
-        sptSet = [False] * self.V
- 
-        for cout in range(self.V):
- 
-            # Pick the minimum distance vertex from
-            # the set of vertices not yet processed.
-            # x is always equal to src in first iteration
-            x = self.minDistance(dist, sptSet)
- 
-            # Put the minimum distance vertex in the
-            # shortest path tree
-            sptSet[x] = True
- 
-            # Update dist value of the adjacent vertices
-            # of the picked vertex only if the current
-            # distance is greater than new distance and
-            # the vertex in not in the shortest path tree
-            for y in range(self.V):
-                if self.graph[x][y] > 0 and sptSet[y] == False and \
-                        dist[y] > dist[x] + self.graph[x][y]:
-                    dist[y] = dist[x] + self.graph[x][y]
- 
-        self.printSolution(dist)
- 
- 
-# Driver's code
+import heapq
+
+def dijkstra(graph, start):
+    priority_queue = [(0, start)]
+    distances = {node: float('infinity') for node in graph}
+    distances[start] = 0
+    
+    while priority_queue:
+        current_distance, current_node = heapq.heappop(priority_queue)
+        
+        if current_distance > distances[current_node]:
+            continue
+        
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+    
+    return distances
+
+def get_graph_from_user():
+    graph = {}
+    nodes = int(input("Enter the number of nodes: "))
+    
+    for _ in range(nodes):
+        node = input("Enter node: ")
+        edges = {}
+        neighbors = int(input(f"Enter the number of neighbors for node {node}: "))
+        
+        for _ in range(neighbors):
+            neighbor, weight = input("Enter neighbor and weight (space-separated): ").split()
+            edges[neighbor] = int(weight)
+            
+        graph[node] = edges
+    
+    return graph
+
 if __name__ == "__main__":
-    g = Graph(9)
-    g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
-               [4, 0, 8, 0, 0, 0, 0, 11, 0],
-               [0, 8, 0, 7, 0, 4, 0, 0, 2],
-               [0, 0, 7, 0, 9, 14, 0, 0, 0],
-               [0, 0, 0, 9, 0, 10, 0, 0, 0],
-               [0, 0, 4, 14, 10, 0, 2, 0, 0],
-               [0, 0, 0, 0, 0, 2, 0, 1, 6],
-               [8, 11, 0, 0, 0, 0, 1, 0, 7],
-               [0, 0, 2, 0, 0, 0, 6, 7, 0]
-               ]
- 
-    g.dijkstra(0)
+    graph = get_graph_from_user()
+    
+    start_node = input("Enter the starting node: ")
+    shortest_distances = dijkstra(graph, start_node)
+    
+    print("Shortest distances from node", start_node + ":")
+    for node, distance in shortest_distances.items():
+        print(f"To {node}: {distance}")
