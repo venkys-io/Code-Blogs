ae6aae07372b302dfc51218fdf29d4dcfe3b24ad
Mouni44 <137180990+Mouni44@users.noreply.github.com>
2023-12-16 21:07:00 +0530

    Update Linear Search.py

--- Easy/Linear Search.py
+++ Easy/Linear Search.py
@@ -1,23 +1,20 @@
-# Function to perform linear search in a list
-def linear_search(array, element):
-    # Check for empty or None array
-    if array is None or not array:
-        print("Invalid input array")
-        return
+def toLowerCase(s: str) -> str:
+    # Initialize an empty string to store the converted characters
+    lowercase = ""
 
-    # Iterate through the array to find the element
-    for idx, val in enumerate(array):
-        # If the element is found, print its index and exit the function
-        if val == element:
-            print(f"The element {element} found at index {idx} in the given array")
-            return
-    
-    # If the element is not found in the array, print a message
-    print(f"The element {element} not found in the given array")
+    # Loop through each character in the input string
+    for char in s:
+        # Check if the character is uppercase
+        if 'A' <= char <= 'Z':
+            # Convert the uppercase character to lowercase using ASCII manipulation
+            lowercase += chr(ord(char) + 32)
+        else:
+            # If the character is not uppercase, keep it unchanged
+            lowercase += char 
 
-# Test list and target element
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-target = 10
+    # Return the converted string with lowercase characters
+    return lowercase
 
-# Perform linear search on the list for the target element
-linear_search(arr, target)
+# Test the function
+string = "UpperCase"
+print(toLowerCase(string))
