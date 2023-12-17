20bf28e3fbf5746d0ad2491f6518be1e441df486
sameera702 <137099599+sameera702@users.noreply.github.com>
2023-12-17 16:31:47 +0530

    Create insertionsort.cpp

---
+++ Sorting/Easy/Insertion Sort/insertionsort.cpp
@@ -0,0 +1,56 @@
+/*Copyrights to venkys.io
+For more information, visite https://venkys.io"/
+
+C++ program to sort a given array in order using Insertion sort algorithm.*/
+
+// Stable : Yes
+// Inplace : Yes
+// Adaptive : Yes
+
+// Space complexity: O(1)
+
+//Time Complexity:O(n^2) in the worst-case scenario, and O(n) in the best-case scenario.
+
+#include<iostream>
+// Include the input/output stream header file
+
+using namespace std;
+// Define the standard namespace for input/output operations
+void VSDInsertionSort(int arr[],int n)
+{
+    for(int i=1;i<n;i++)
+		{
+        int temp=arr[i];
+        int j=i;
+        while(j>0 && arr[j-1]>temp){
+            arr[j]=arr[j-1];
+            j--;
+        }
+        arr[j]=temp;
+// Place the value stored in temp at the position j, effectively inserting the value in its correct place
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+// Move to the next line after printing all elements
+}
+
+int main(){
+    int n;
+
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    VSDInsertionSort(arr, n);
+// Call the VSDInsertionSort function to sort the array
+    return 0;
+// Return 0 to indicate successful execution of the program
+}
