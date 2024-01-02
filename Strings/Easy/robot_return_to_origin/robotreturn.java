831ef6c8a6fd9f6d44fe1622be434a0fd6d3117d
geethamsh_rao <137193113+geethamshraod@users.noreply.github.com>
2024-01-02 22:06:00 +0530

    Update robotreturn.java

--- Strings/Easy/robot_return_to_origin/robotreturn.java
+++ Strings/Easy/robot_return_to_origin/robotreturn.java
@@ -1,36 +1,41 @@
 import java.util.Scanner;
 
 public class robotreturn {
+    public static void main(String[] args) {
+        try (// Get user input for directions (case-insensitive)
+                Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter the directions: ");
+            String z = scanner.nextLine().toUpperCase(); // Convert input to uppercase for case-insensitivity
 
-    public static boolean isRobotReturnToOrigin(String input) {
-        int x = 0, y = 0;
-        for (char i : input.toCharArray()) {
-            if (i == 'U') {
-                y += 1;
-            } else if (i == 'D') {
-                y -= 1;
-            } else if (i == 'R') {
-                x += 1;
-            } else if (i == 'L') {
-                x -= 1;
-            } else {
-                System.out.println("Wrong input");
-                return false;
-            }
+            // Call the function and print the result
+            System.out.println(robotReturnToOrigin(z)); // returns True if the robot returns to the origin else returns False
+                                                        
+
         }
-        return x == 0 && y == 0;
     }
 
-    public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter the directions: ");
-            String directions = scanner.nextLine().toUpperCase();
+    static boolean robotReturnToOrigin(String input) {
+        // Initialize the coordinates to (0, 0)
+        int x = 0, y = 0;
 
-            if (isRobotReturnToOrigin(directions)) {
-                System.out.println("Robot gotcha");
+        // Iterate through each character in the input string
+        for (char i : input.toCharArray()) {
+            // Update coordinates based on the current direction
+            if (i == 'U') { // U denotes "UP" and it moves towards top from the origin(y-axis)
+                y += 1;
+            } else if (i == 'D') { // D denotes "DOWN" and it moves bottom from the origin(y-axis)
+                y -= 1;
+            } else if (i == 'R') { // R denotes "RIGHT" and it moves right hand side from the origin(x-axis)
+                x += 1;
+            } else if (i == 'L') { // L denotes "LEFT" and it moves left hand side from the origin(x-axis)
+                x -= 1;
             } else {
-                System.out.println("Lost your robot");
+                // Handle invalid input characters other than "R,U,L,D"
+                System.out.println("Wrong input");
             }
         }
+
+        // Check if the final coordinates are at the origin (0, 0)
+        return x == 0 && y == 0;
     }
 }
