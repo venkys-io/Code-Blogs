5b3a4d05dba5757aac3b88683b5499a649f01116
geethamsh_rao <137193113+geethamshraod@users.noreply.github.com>
2024-01-02 22:07:53 +0530

    Update robotreturn.cpp

--- Strings/Easy/robot_return_to_origin/robotreturn.cpp
+++ Strings/Easy/robot_return_to_origin/robotreturn.cpp
@@ -1,55 +1,56 @@
 #include <iostream>
-#include <string>
-using namespace std;
+#include <cctype> // For toupper function
 
-bool robot_return_to_origin(const string &input) // Function to check if the robot returns to the origin
+bool robotReturnToOrigin(const std::string &input)
 {
+    // Initialize the coordinates to (0, 0)
     int x = 0, y = 0;
-    for (char i : input) // Iterate through each character in the input string
+
+    // Iterate through each character in the input string
+    for (char i : input)
     {
-        if (i == 'U') // Update the position based on the current character
-        {
+        // Update coordinates based on the current direction
+        if (i == 'U')
+        { // U denotes "UP" and it moves towards top from the origin(y-axis)
             y += 1;
         }
         else if (i == 'D')
-        {
+        { // D denotes "DOWN" and it moves bottom from the origin(y-axis)
             y -= 1;
         }
         else if (i == 'R')
-        {
+        { // R denotes "RIGHT" and it moves right hand side from the origin(x-axis)
             x += 1;
         }
         else if (i == 'L')
-        {
+        { // L denotes "LEFT" and it moves left hand side from the origin(x-axis)
             x -= 1;
         }
         else
         {
-            cout << "wrong input" << endl; // Handle invalid input
-            return false;
+            // Handle invalid input characters other than "R,U,L,D"
+            std::cout << "Wrong input" << std::endl;
         }
     }
-    return x == 0 && y == 0; // Check if the final position is the origin (0, 0)
+
+    // Check if the final coordinates are at the origin (0, 0)
+    return x == 0 && y == 0;
 }
 
 int main()
 {
-    string directions;
-    cout << "Enter the directions: ";
-    cin >> directions; // Read user input
-    for (char &c : directions)
+    // Get user input for directions (case-insensitive)
+    std::string z;
+    std::cout << "Enter the directions: ";
+    std::cin >> z;
+
+    // Convert input to uppercase for case-insensitivity
+    for (char &c : z)
     {
-        c = toupper(c); // Convert the input string to uppercase for case-insensitive comparison
+        c = std::toupper(c);
     }
 
-    if (robot_return_to_origin(directions)) // Convert the input string to uppercase for case-insensitive comparison
-    {
-        cout << "Robot gotcha" << endl;
-    }
-    else
-    {
-        cout << "Lost your robot" << endl;
-    }
-
+    // Call the function and print the result
+    std::cout << (robotReturnToOrigin(z) ? "true" : "false") << std::endl; // returns true if the robot returns to the origin else returns false
     return 0;
 }
