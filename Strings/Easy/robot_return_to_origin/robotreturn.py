99d78d87fe739be435acb108f7ce33e6d77d469a
geethamsh_rao <137193113+geethamshraod@users.noreply.github.com>
2024-01-02 22:05:20 +0530

    Update robotreturn.py

--- Strings/Easy/robot_return_to_origin/robotreturn.py
+++ Strings/Easy/robot_return_to_origin/robotreturn.py
@@ -1,24 +1,29 @@
-def robot_return_to_origin(input):# Function to check if the robot returns to the origin
+def robot_return_to_origin(input):
+    # Initialize the coordinates to (0, 0)
     x = y = 0
-    for i in input:# Update the position based on the current character
-        if i == "U":
+
+    # Iterate through each character in the input string
+    for i in input:
+        # Update coordinates based on the current direction
+        if i == "U": #U denotes "UP" and it moves towards top from the origin(y-axis)
             y += 1
-        elif i == "D":
+        elif i == "D": #D denotes "DOWN" and it moves bottom from the origin(y-axis)
             y -= 1
-        elif i == "R":
+        elif i == "R": #R denotes "RIGHT" and it moves right hand side from the origin(x-axis)
             x += 1
-        elif i == "L":
+        elif i == "L": #L denotes "LEFT" and it moves left hand side from the origin(x-axis)
             x -= 1
         else:
-            print("wrong input")
+            # Handle invalid input characters other than "R,U,L,D"
+            print("Wrong input")
+
+    # Check if the final coordinates are at the origin (0, 0)
     return x == 0 and y == 0
 
-z = input("Enter the directions: ")# Get user input for robot directions
-z=z.upper()
-if robot_return_to_origin(z):# Check if the robot returns to the origin
-    print("Got the Robot")
-else:
-    print("lost your robot")
+# Get user input for directions (case-insensitive)
+z = input("Enter the directions: ")
+z = z.upper()  # Convert input to uppercase for case-insensitivity
 
-
-
+# Call the function and print the result
+print(robot_return_to_origin(z))
+#returns True if the robot returns to the origin else returns False
