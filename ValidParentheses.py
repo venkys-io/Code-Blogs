e6cfdd8725b64a6e54ffc90177878f167b8fea92
Sneha Vellelath <129183233+vellsneha@users.noreply.github.com>
2024-03-26 01:02:03 +0530

    Create ValidParentheses.py

---
+++ ValidParentheses.py
@@ -0,0 +1,23 @@
+# Copyrights to venkys.io
+# For more information, visit https://venkys.io
+def isValidParanthesis(string:str)->bool:
+
+    stack=list()
+
+    for char in string:
+        if char=="(" or char=="{" or char=="[":
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            top=stack.pop()
+            if char==")" and top!="(":
+                return False
+            if char=="}" and top!="{":
+                return False
+            if char=="]" and top!="[":
+                return False
+    return len(stack)==0
+        
+input= input()
+print(isValidParanthesis(input))
